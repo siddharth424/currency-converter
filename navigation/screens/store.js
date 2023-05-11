15e8a5fc1b5e2ca9b24158bbc090cb5f8799195e
Siddharth <siddharthmaurya887@gmail.com>
2023-05-11 18:01:59 +0530

    added comments

--- navigation/screens/store.js
+++ navigation/screens/store.js
@@ -1,28 +1,28 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+// Store data in AsyncStorage
 export const storeData = async (key, value) => {
   try {
-      await AsyncStorage.setItem(key, JSON.stringify(value));
-      
+    await AsyncStorage.setItem(key, JSON.stringify(value));
   } catch (e) {
     console.log('Error storing data:', e);
   }
 };
 
+// Retrieve data from AsyncStorage
 export const getData = async (key) => {
   try {
-      const value = await AsyncStorage.getItem(key);
+    const value = await AsyncStorage.getItem(key);
     return value !== null ? JSON.parse(value) : null;
   } catch (e) {
     console.log('Error retrieving data:', e);
   }
 };
 
-
+// Remove data from AsyncStorage
 export const removeData = async (key) => {
-    try {
-        await AsyncStorage.removeItem(key);
-
+  try {
+    await AsyncStorage.removeItem(key);
   } catch (e) {
     console.log('Error removing data:', e);
   }
