964f9fc28563f49db2bceb581e0865c22c666967
Siddharth <siddharthmaurya887@gmail.com>
2023-05-11 17:59:13 +0530

    fixed UI and loading

--- navigation/screens/CurrencyExchange.js
+++ navigation/screens/CurrencyExchange.js
@@ -14,18 +14,19 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import NetInfo from "@react-native-community/netinfo";
 
 const CurrencyExchange = () => {
-  const [fromCurrency, setFromCurrency] = useState("USD");
-  const [toCurrency, setToCurrency] = useState("EUR");
-  const [exchangeRate, setExchangeRate] = useState(0);
-  const [amount, setAmount] = useState(1);
-  const [currencies, setCurrencies] = useState([]);
-  const [isBookmarked, setIsBookmarked] = useState(false);
-  const [backuplist, setbackuplist] = useState([]);
-  const [isOnline, setIsOnline] = useState(true);
-  
+  const [fromCurrency, setFromCurrency] = useState("USD"); // State for the selected "from" currency
+  const [toCurrency, setToCurrency] = useState("EUR"); // State for the selected "to" currency
+  const [exchangeRate, setExchangeRate] = useState(0); // State for the exchange rate
+  const [amount, setAmount] = useState(1); // State for the amount to convert
+  const [currencies, setCurrencies] = useState([]); // State for the available currencies
+  const [isBookmarked, setIsBookmarked] = useState(false); // State to track if the currency pair is bookmarked
+  const [backuplist, setbackuplist] = useState([]); // State for the backup currency rates
+  const [isOnline, setIsOnline] = useState(true); // State to track online/offline status
 
+  // Function to toggle bookmark for the currency pair
   const toggleBookmark = async () => {
     if (isBookmarked) {
+      // Remove the currency pair from the watchlist
       try {
         const storedCurrencies = await AsyncStorage.getItem("watchlist");
         const currenciesArray = JSON.parse(storedCurrencies);
@@ -43,6 +44,7 @@ const CurrencyExchange = () => {
         console.log(error);
       }
     } else {
+      // Add the currency pair to the watchlist
       try {
         const storedCurrencies = await AsyncStorage.getItem("watchlist");
         const currenciesArray = JSON.parse(storedCurrencies) || [];
@@ -58,6 +60,7 @@ const CurrencyExchange = () => {
     }
   };
 
+  // Function to load the watchlist and check if the currency pair is bookmarked
   const loadWatchlist = async () => {
     try {
       const storedCurrencies = await AsyncStorage.getItem("watchlist");
@@ -76,42 +79,61 @@ const CurrencyExchange = () => {
       console.log(error);
     }
   };
-  
 
+  // Function to convert the currency based on the selected currencies and amount
   const convertCurrency = () => {
     let rate = backuplist[toCurrency] / backuplist[fromCurrency];
-    let result = (amount * rate).toFixed(2);
+    if (isNaN(amount)) {
+      setAmount(1);
+    }
+    if (isNaN(rate))
+    {
+      rate = 1;
+      return "Loading...";
+    }
+    let result = (amount * rate).toFixed(4);
     return result;
   };
 
+  // Fetch the list of available currencies and the exchange rate on component mount or when the "to" currency changes
   useEffect(() => {
     const fetchCurrencies = async () => {
       try {
+        // Check the network connection status
         const isConnected = await NetInfo.fetch().then(
           (state) => state.isConnected
         );
         setIsOnline(isConnected);
 
+        // Fetch the exchange rate data for USD
         const response = await fetch(
           `https://v6.exchangerate-api.com/v6/89cc9cc4efde77f6f4a8dadc/latest/USD`
         );
         const data = await response.json();
+
+        // Set the available currencies and the exchange rate for the selected "to" currency
         setCurrencies(Object.keys(data.conversion_rates));
         setExchangeRate(data.conversion_rates[toCurrency]);
       } catch (error) {
         console.log(error);
       }
     };
+
     fetchCurrencies();
   }, [toCurrency]);
 
+  //Fetch exchange rates of from currency
   useEffect(() => {
     const fetchExchangeRate = async () => {
+      // Load watchlist and check if the currency pair is bookmarked
       loadWatchlist();
+
       try {
         const converter = await fetch(
           `https://v6.exchangerate-api.com/v6/89cc9cc4efde77f6f4a8dadc/latest/USD`
         );
+
+        // Fetch the exchange rate for the selected "from" currency
         const response = await fetch(
           `https://v6.exchangerate-api.com/v6/89cc9cc4efde77f6f4a8dadc/latest/${fromCurrency}`
         );
@@ -119,10 +141,11 @@ const CurrencyExchange = () => {
         const templist = await converter.json();
         setbackuplist(templist.conversion_rates);
         setExchangeRate(data.conversion_rates[toCurrency]);
+
+        // Retrieve the existing history data or initialize an empty array
         const history = (await getData("history")) || [];
 
         // Append the current conversion data to the history array
-
         history.push({ fromCurrency, toCurrency, exchangeRate });
 
         // Save the updated history array back to AsyncStorage
@@ -131,9 +154,11 @@ const CurrencyExchange = () => {
         console.log(error);
       }
     };
+
     fetchExchangeRate();
   }, [fromCurrency, toCurrency]);
 
+  //Check for online or offline
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener((state) => {
       setIsOnline(state.isConnected);
@@ -162,6 +187,7 @@ const CurrencyExchange = () => {
         keyboardType="numeric"
       />
       <View style={styles.pickerContainer}>
+        {/* From Currency Picker */}
         <Picker
           style={styles.picker}
           selectedValue={fromCurrency}
@@ -172,6 +198,7 @@ const CurrencyExchange = () => {
           ))}
         </Picker>
 
+        {/* To Currency Picker */}
         <Picker
           style={styles.picker}
           selectedValue={toCurrency}
@@ -181,12 +208,14 @@ const CurrencyExchange = () => {
             <Picker.Item key={index} label={currency} value={currency} />
           ))}
         </Picker>
+
+        {/* Bookmark Button */}
         <View style={styles.bookmarkContainer}>
           <TouchableOpacity onPress={toggleBookmark}>
             <Ionicons
               name={isBookmarked ? "bookmark" : "bookmark-outline"}
               size={30}
-              color="black"
+              color="darkgreen"
             />
           </TouchableOpacity>
         </View>
@@ -252,7 +281,7 @@ const styles = StyleSheet.create({
   },
   result: {
     fontSize: 25,
-    marginTop: 200,
+    marginTop: 0,
     fontWeight: "bold",
     color: "#1d3627",
   },
