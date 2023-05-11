8607dd65bf91e2c05114ee0bf076f49389843e8b
Siddharth <siddharthmaurya887@gmail.com>
2023-05-11 18:00:36 +0530

    added comments

--- navigation/screens/History.js
+++ navigation/screens/History.js
@@ -1,44 +1,39 @@
 import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  FlatList,
-  TouchableOpacity,
-  StyleSheet,
-} from "react-native";
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from "react-native";
 import { getData, removeData } from "./store";
 
 const History = () => {
   const [history, setHistory] = useState([]);
 
-
-    const getHistory = async () => {
-      try {
-        const storedHistory = await getData("history");
-        if (storedHistory) {
-          const filteredHistory = storedHistory.filter(
-            (item) => item.exchangeRate !== 0
-          );
-          setHistory(filteredHistory);
-        }
-      } catch (error) {
-        console.log("Error getting history:", error);
+  // Function to retrieve history data from AsyncStorage
+  const getHistory = async () => {
+    try {
+      const storedHistory = await getData("history");
+      if (storedHistory) {
+        // Filter out items with exchangeRate = 0
+        const filteredHistory = storedHistory.filter((item) => item.exchangeRate !== 0);
+        setHistory(filteredHistory);
       }
-    };
+    } catch (error) {
+      console.log("Error getting history:", error);
+    }
+  };
 
   useEffect(() => {
-    const interval = setInterval(getHistory, 100); // Call loadWatchlist every 5 seconds
+    // Call getHistory function every 100 milliseconds
+    const interval = setInterval(getHistory, 100);
 
     return () => {
       clearInterval(interval); // Clear the interval when the component unmounts
     };
   }, []);
 
+  // Function to clear the history data
   const clearHistory = async () => {
     try {
       await removeData("history");
       console.log("History data removed");
-      setHistory([]);
+      setHistory([]); // Clear the history state
       console.log("History state updated");
     } catch (error) {
       console.log("Error clearing history:", error);
@@ -47,28 +42,22 @@ const History = () => {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        onPress={clearHistory}
-        style={{ alignItems: "flex-end" }}
-      >
+      <TouchableOpacity onPress={clearHistory} style={{ alignItems: "flex-end" }}>
         <Text>Clear History</Text>
       </TouchableOpacity>
 
       {history.length === 0 ? (
+        // Render a message when no history is available
         <Text style={styles.message}>No history available</Text>
       ) : (
         <FlatList
-          data={history}
+          data={history.reverse()} // Reverse the history array to display latest items first
           keyExtractor={(item, index) => index.toString()}
           renderItem={({ item }) => (
             <View style={styles.item}>
-              <Text
-                style={styles.itemText}
-              >{`From: ${item.fromCurrency}`}</Text>
+              <Text style={styles.itemText}>{`From: ${item.fromCurrency}`}</Text>
               <Text style={styles.itemText}>{`To: ${item.toCurrency}`}</Text>
-              <Text
-                style={styles.itemText}
-              >{`Exchange rate: ${item.exchangeRate}`}</Text>
+              <Text style={styles.itemText}>{`Exchange rate: ${item.exchangeRate}`}</Text>
             </View>
           )}
         />
@@ -81,7 +70,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: "#fff",
+    backgroundColor: "#dff0e6",
   },
   header: {
     fontSize: 24,
