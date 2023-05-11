d448a6747defdde5828370ad53bc05e3ba4dc4b8
Siddharth <siddharthmaurya887@gmail.com>
2023-05-11 17:59:00 +0530

    fixed UI and loading

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { StyleSheet,View} from "react-native";
+import { StyleSheet, View } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -11,21 +11,22 @@ import RateChart from "./screens/RateChart";
 import WatchList from "./screens/WatchList";
 import History from "./screens/History";
 
-//Screen names
+// Screen names
 const exchangeName = "Currency Exchange";
 const chartName = "Rate Chart";
-const history = "History"
+const history = "History";
 const watchlistName = "Watchlist";
 
 const Tab = createBottomTabNavigator();
 console.disableYellowBox = true;
 
-
 function MainContainer() {
   return (
-    <NavigationContainer >
+    // Navigation container to wrap the tab navigator
+    <NavigationContainer>
       <Tab.Navigator
         initialRouteName={exchangeName}
+        // Configure options for each screen in the tab navigator
         screenOptions={({ route }) => ({
           tabBarActiveTintColor: "tomato",
           tabBarInactiveTintColor: "grey",
@@ -44,27 +45,29 @@ function MainContainer() {
             let iconName;
             let rn = route.name;
 
+            // Determine the appropriate icon name based on the screen name and focus state
             if (rn === exchangeName) {
               iconName = focused ? "swap-horizontal" : "swap-horizontal-outline";
             } else if (rn === chartName) {
               iconName = focused ? "bar-chart" : "bar-chart-outline";
             } else if (rn === watchlistName) {
               iconName = focused ? "bookmark" : "bookmark-outline";
-            } else if (rn == history) {
+            } else if (rn === history) {
               iconName = focused ? "timer" : "timer-outline";
             }
 
-            // You can return any component that you like here!
+            // Return the Ionicons component with the selected icon name and styling
             return <Ionicons name={iconName} size={size} color={color} />;
           },
         })}
       >
+        {/* Define the screens and their components */}
         <Tab.Screen name={exchangeName} component={CurrencyExchange} />
         <Tab.Screen name={chartName} component={RateChart} />
         <Tab.Screen name={watchlistName} component={WatchList} />
-        <Tab.Screen name={history} component={History}/>
+        <Tab.Screen name={history} component={History} />
       </Tab.Navigator>
-      </NavigationContainer>
+    </NavigationContainer>
   );
 }
 
